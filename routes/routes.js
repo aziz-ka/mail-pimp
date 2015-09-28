5915731e4a87e987a6976bd5e693df53402f3fe3
aziz-ka <hasanov.aziz@gmail.com>
2015-09-28 11:04:43 -0400

    working on email attachments

--- routes/routes.js
+++ routes/routes.js
@@ -1,11 +1,16 @@
 var express = require('express');
 var router = express.Router();
 var chalk = require("chalk");
+var util = require("util");
+var formidable = require("formidable");
 var passport = require("passport");
 var base64url = require("base64-url");
 var gmail = require("googleapis").gmail("v1");
 var scopes = ["https://www.googleapis.com/auth/userinfo.email", "https://mail.google.com"];
 var auth = require("../auth.js");
+var multer = require("multer");
+var upload = multer({dest:'./uploads/'});
+var fs = require("fs-extra");
 
 router.get('/', function(req, res) {
   res.render('index', { title: 'Contactly Emailer' });
@@ -22,25 +27,53 @@ router.get("/email", function(req, res, next) {
   res.render("email", {user: req.user});
 });
 
-router.post("/send", function(req, res, next) {
+router.post("/send", upload.single("attachment"), function(req, res, next) {
   var tokens = auth.tokens();
+  console.log(req.file);
 
-  sendEmail(JSON.parse(req.user), req.body, tokens);
+  encodeMessage(JSON.parse(req.user), req.body, req.file, tokens);
   res.redirect("/");
 });
 
 module.exports = router;
 
-function sendEmail(user, email, tokens) {
-  // insert 3D before opening qoutes in html tags
-  var message = "From: <" + user.emails[0].value +
-                ">\r\nTo: <" + email.address +
-                ">\r\nSubject: " + email.subject +
-                "\r\nContent-Type: text/html; charset=utf-8" +
-                "\r\nMIME-Version: 1.0" +
-                "\r\nContent-Transfer-Encoding: quoted-printable\r\n\r\n" +
-                email.message;
+function encodeMessage(user, email, file, tokens) {
+  var message = "";
+  message += "From: <" + user.emails[0].value + ">\r\n";
+  message += "To: <" + email.address + ">\r\n";
+  message += "Subject: " + email.subject + "\r\n";
+  message += "MIME-Version: 1.0\r\n";
 
+  if(!file) {
+    message += "Content-Type: text/html; charset='utf-8'\r\n\r\n";
+    message += email.message;
+    sendEmail(message, tokens);
+  } else {
+    message += "Content-Type: message/rfc822; boundary='the_end'\r\n\r\n";
+
+    message += "--the_end\r\n";
+    message += "MIME-Version: 1.0\r\n";
+    message += "Content-Transfer-Encoding: 7bit\r\n";
+    message += "Content-Type: text/html; charset='utf-8'\r\n\r\n";
+    message += email.message + "\r\n\r\n";
+
+    message += "--the_end\r\n";
+    message += "MIME-Version: 1.0\r\n";
+    message += "Content-Type: " + file.mimetype + "\r\n";
+    message += "Content-Transfer-Encoding: base64\r\n";
+    message += "Content-Disposition: attachment; filename='" + file.filename + "'\r\n\r\n";
+
+    fs.readFile(file.path, function(err, fileData) {
+      fileData = base64url.encode(fileData);
+      message += fileData + "\r\n";
+      message += "--the_end--";
+      sendEmail(message, tokens, file, fileData);
+    });
+  }
+  // console.log(file);
+}
+
+function sendEmail(message, tokens, file, fileData) {
   var msgEncoded = base64url.encode(message);
 
   console.log(chalk.bgYellow(message));
